a7586b717c205d0800a35acc8acf72813e763e2d
Hugge25 <hugo.paulsson@hotmail.com>
2023-01-20 10:31:50 +0100

    Add project files.

---
+++ uppgift 5.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Upgift_5_1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur många meningar vill du skriva?");
+            int antal = int.Parse(Console.ReadLine());
+            string[] meningar = new string[antal];
+
+            for (int i = 0; i < antal; i++)
+            {
+                Console.WriteLine("skriv in en mening");
+                meningar[i] = Console.ReadLine();
+
+            }
+            Console.WriteLine("Här är dina meningar");
+            for (int i = 0; i < antal; i++)
+            {
+                Console.WriteLine($"\n{meningar[i]}");
+            }
+
+
+            
+
+        }
+    }
+
+}
